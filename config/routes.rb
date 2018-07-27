a9ff6d3f4b652e5ee129e2eeb36b29675f70b3a1
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-27 10:48:19 -0600

    User can sign out

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   get '/auth/google_oauth2', as: :google_login
   get '/auth/google_oauth2/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
 end
