4e3f6dc951b4c0a22fa7d25f6b9332476d59afa1
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-27 10:21:38 -0600

    Add comment spec

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,5 +4,5 @@ describe Author do
   it { is_expected.to validate_presence_of :name }
 
   it { is_expected.to have_many :posts }
-  xit { is_expected.to have_many :comments }
+  it { is_expected.to have_many :comments }
 end
